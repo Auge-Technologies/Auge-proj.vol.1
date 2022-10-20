fc85f2fb550fbf30d105f7b852b1ebf2d021b20a
haraldpaaske <haraldpaaske@gmail.com>
2022-10-20 13:25:17 +0200

    starten på interaction

--- game/pages/skill-dialogue/[skillId].js
+++ game/pages/skill-dialogue/[skillId].js
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import client from "../../lib/client";
 import { useRouter } from "next/router";
+import styles from "./skilldialogue.module.scss"
 
-const SkillDialogue = ({ dialogue, skillId }) => {
+const SkillDialogue = ({ dialogue, skillId, wizard }) => {
   const router = useRouter();
+  const [dialogueText, setDialogueText] = useState(dialogue[0])
+  
+  const characterImages = [
+    '/characters/'+wizard.characterType+'/'+wizard.characterType+'1.png',
+    '/characters/'+ wizard.characterType+'/'+wizard.characterType+'2.png',
+    '/characters/'+ wizard.characterType+'/'+wizard.characterType+'3.png',
+    '/characters/'+wizard.characterType+'/'+wizard.characterType+'4.png']
+
+    const [imageURI, setImageURI] = useState(characterImages[0])
+
+  const nextDialogue = () => {
+    var index  = dialogue.indexOf(dialogueText)
+    if(index === -1) index = 0
+    setDialogueText(dialogue[index+1])
+    if(index%2 == 0){
+      var imgIndex = characterImages.indexOf(imageURI)
+      if(imgIndex+1 == characterImages.length) imgIndex = 0
+      console.log(imgIndex)
+      setImageURI(characterImages[imgIndex+1])
+    }
+
+
+    
+  };
+
   return (
     <div>
-      <ul>
-        {dialogue.map((text) => (
-          <li>{text}</li>
-        ))}
-      </ul>
       <button onClick={() => router.push("/" + skillId)}>Ferdig</button>
+      <div className={styles.wizard}>
+        <img src={imageURI}></img>
+      </div>
+      <div className={styles.textbubble}>
+        <h2 className={styles.dialogue}>{dialogueText}</h2>
+        <button onClick={nextDialogue}>Next</button>
+      </div>
+      <div className={styles.questions}>
+        <button>one</button>
+        <button>another</button>
+        <button>another again</button>
+      </div>
     </div>
   );
 };
@@ -37,7 +70,8 @@ export async function getStaticProps(context) {
     (await client.fetch(
       `
       *[_type == "skill" && _id == $skillId] {
-        dialogue
+        dialogue, 
+        wizard -> {name, characterType}
       }[0]
     `,
       { skillId }
