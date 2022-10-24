f415a892b0bad59d304aca244b5cac223619ef4a
haraldpaaske <haraldpaaske@gmail.com>
2022-10-24 16:36:26 +0200

    Update Map.js

--- game/features/map/Map.js
+++ game/features/map/Map.js
@@ -15,22 +15,6 @@ export const Map = ({
       <div className={styles.mapWrapper}>
         <Character route={route} />
         <p className={styles.name}>{name}</p>
-        <div className={cn(styles.map, styles.left)}>
-          <Image
-            src={`/mapTiles/left-${inputConnectionCount}-foreground.png`}
-            alt={`left-${inputConnectionCount}-foreground`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition={"right"}
-          />{" "}
-          <Image
-            src={`/mapTiles/left-${inputConnectionCount}-background.png`}
-            alt={`left-${inputConnectionCount}-background`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition={"right"}
-          />
-        </div>
         <div className={cn(styles.map, styles.right)}>
           <Image
             src={`/mapTiles/right-${outputConnectionCount}-foreground.png  `}
@@ -45,6 +29,22 @@ export const Map = ({
             layout="fill"
             objectFit="cover"
             objectPosition={"left"}
+          />
+        </div>
+        <div className={cn(styles.map, styles.left)}>
+          <Image
+            src={`/mapTiles/left-${inputConnectionCount}-foreground.png`}
+            alt={`left-${inputConnectionCount}-foreground`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
+          />{" "}
+          <Image
+            src={`/mapTiles/left-${inputConnectionCount}-background.png`}
+            alt={`left-${inputConnectionCount}-background`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
           />
         </div>
       </div>
