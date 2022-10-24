c6471f8dbe8a002846ad1ab52715775978122d8b
haraldpaaske <haraldpaaske@gmail.com>
2022-10-24 17:21:25 +0200

    Update Map.js

--- game/features/map/Map.js
+++ game/features/map/Map.js
@@ -15,26 +15,6 @@ export const Map = ({
       <div className={styles.mapWrapper}>
         <Character route={route} />
         <p className={styles.name}>{name}</p>
-        <div className={cn(styles.map, styles.left)}>
-          <Image
-            src={`/mapTiles/left/left-${inputConnectionCount}-foreground.png`}
-            alt={`left-${inputConnectionCount}-foreground`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition={"right"}
-            priority
-            crossOrigin="anonymous"
-          />{" "}
-          <Image
-            src={`/mapTiles/left/left-${inputConnectionCount}-background.png`}
-            alt={`left-${inputConnectionCount}-background`}
-            layout="fill"
-            objectFit="cover"
-            objectPosition={"right"}
-            priority
-            crossOrigin="anonymous"
-          />
-        </div>
         <div className={cn(styles.map, styles.right)}>
           <Image
             src={`/mapTiles/right/right-${outputConnectionCount}-foreground.png  `}
@@ -43,7 +23,6 @@ export const Map = ({
             objectFit="cover"
             objectPosition={"left"}
             priority
-            crossOrigin="anonymous"
           />
           <Image
             src={`/mapTiles/right/right-${outputConnectionCount}-background.png  `}
@@ -52,7 +31,24 @@ export const Map = ({
             objectFit="cover"
             objectPosition={"left"}
             priority
-            crossOrigin="anonymous"
+          />
+        </div>
+        <div className={cn(styles.map, styles.left)}>
+          <Image
+            src={`/mapTiles/left/left-${inputConnectionCount}-foreground.png`}
+            alt={`left-${inputConnectionCount}-foreground`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
+            priority
+          />{" "}
+          <Image
+            src={`/mapTiles/left/left-${inputConnectionCount}-background.png`}
+            alt={`left-${inputConnectionCount}-background`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
+            priority
           />
         </div>
       </div>
