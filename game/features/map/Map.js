928080c7af1b69ae685cbb377853b49e61f17d2c
haraldpaaske <haraldpaaske@gmail.com>
2022-10-24 15:44:37 +0200

    Update Map.js

--- game/features/map/Map.js
+++ game/features/map/Map.js
@@ -11,45 +11,47 @@ export const Map = ({
   skillId,
 }) => {
   return (
-    <div className={styles.mapWrapper}>
-      <Character route={route} />
-      <p className={styles.name}>{name}</p>
-      <div className={cn(styles.map, styles.left)}>
-        <Image
-          src={`/mapTiles/left-${inputConnectionCount}-foreground.png`}
-          alt={`left-${inputConnectionCount}-foreground`}
-          layout="fill"
-          objectFit="cover"
-          objectPosition={"right"}
-          priority
-        />{" "}
-        <Image
-          src={`/mapTiles/left-${inputConnectionCount}-background.png`}
-          alt={`left-${inputConnectionCount}-background`}
-          layout="fill"
-          objectFit="cover"
-          objectPosition={"right"}
-          priority
-        />
+    <>
+      <div className={styles.mapWrapper}>
+        <Character route={route} />
+        <p className={styles.name}>{name}</p>
+        <div className={cn(styles.map, styles.left)}>
+          <Image
+            src={`/mapTiles/left-${inputConnectionCount}-foreground.png`}
+            alt={`left-${inputConnectionCount}-foreground`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
+            priority
+          />{" "}
+          <Image
+            src={`/mapTiles/left-${inputConnectionCount}-background.png`}
+            alt={`left-${inputConnectionCount}-background`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"right"}
+            priority
+          />
+        </div>
+        <div className={cn(styles.map, styles.right)}>
+          <Image
+            src={`/mapTiles/right-${outputConnectionCount}-foreground.png  `}
+            alt={`right-${outputConnectionCount}-foreground`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"left"}
+            priority
+          />
+          <Image
+            src={`/mapTiles/right-${outputConnectionCount}-background.png  `}
+            alt={`right-${outputConnectionCount}-background`}
+            layout="fill"
+            objectFit="cover"
+            objectPosition={"left"}
+            priority
+          />
+        </div>
       </div>
-      <div className={cn(styles.map, styles.right)}>
-        <Image
-          src={`/mapTiles/right-${inputConnectionCount}-foreground.png  `}
-          alt={`right-${outputConnectionCount}-foreground`}
-          layout="fill"
-          objectFit="cover"
-          objectPosition={"left"}
-          priority
-        />
-        <Image
-          src={`/mapTiles/right-${inputConnectionCount}-background.png  `}
-          alt={`right-${outputConnectionCount}-background`}
-          layout="fill"
-          objectFit="cover"
-          objectPosition={"left"}
-          priority
-        />
-      </div>
-    </div>
+    </>
   );
 };
