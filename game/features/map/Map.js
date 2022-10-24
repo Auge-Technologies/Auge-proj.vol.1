7165206386a09da796cbcad82c2617d7e1797ead
haraldpaaske <haraldpaaske@gmail.com>
2022-10-24 10:56:57 +0200

    Update Map.js

--- game/features/map/Map.js
+++ game/features/map/Map.js
@@ -10,6 +10,7 @@ export const Map = ({
   route,
   skillId,
 }) => {
+  console.log(outputConnectionCount);
   return (
     <div className={styles.mapWrapper}>
       <Character route={route} />
@@ -34,7 +35,7 @@ export const Map = ({
       </div>
       <div className={cn(styles.map, styles.right)}>
         <Image
-          src={`/mapTiles/right-${outputConnectionCount}-foreground.png  `}
+          src={`/mapTiles/right-${inputConnectionCount}-foreground.png  `}
           alt={`right-${inputConnectionCount}-foreground`}
           layout="fill"
           objectFit="cover"
@@ -42,7 +43,7 @@ export const Map = ({
           priority
         />
         <Image
-          src={`/mapTiles/right-${outputConnectionCount}-background.png  `}
+          src={`/mapTiles/right-${inputConnectionCount}-background.png  `}
           alt={`right-${inputConnectionCount}-background`}
           layout="fill"
           objectFit="cover"
